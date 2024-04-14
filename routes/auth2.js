81070978fc41985c3aa9fd58665b24e6f503c9f8
chaudhary <164286972+0-Chaudhary-0@users.noreply.github.com>
2024-04-14 17:39:16 +0100

    Update auth2.js

--- routes/auth2.js
+++ routes/auth2.js
@@ -1,1 +1,30 @@
 
+// Login API Is Here
+router.post('/signin', async (req, res) => {
+    let { email, password } = await req.body;
+    let user = await User.findOne({ email });
+
+    if (user) {
+
+        // Decrypt
+        const bytes = CryptoJS.AES.decrypt(user.password, "#@chaudhary@#");
+        const decryptedPassword = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+        if (user.email == email && password == decryptedPassword) {
+
+            // JWT Token
+            const token = jwt.sign({ email }, "#@chaudhary@#");
+            res.send({
+                success: true,
+                message: "Successfully Logged In",
+                token: token
+            });
+        } else {
+            res.send({ success: false, message: `Invalid Input` });
+        }
+    } else {
+        res.send({ success: false, message: `User Not Found` });
+    }
+})
+
+module.exports = router
