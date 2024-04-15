e31d8e098b743a7a80bb973cc2feed104c096bda
chaudhary <164286972+0-Chaudhary-0@users.noreply.github.com>
2024-04-15 08:32:37 +0500

    Update signin.js

--- routes/signin.js
+++ routes/signin.js
@@ -1,1 +1,39 @@
+const express = require('express')
+const User = require('../models/User')
+const CryptoJS = require('crypto-js')
+const jwt = require("jsonwebtoken");
+const router = express.Router()
 
+
+// Login API Is Here
+router.post('/signin', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    
+    let { email, password } = await req.body;
+    let user = await User.findOne({ email });
+
+    if (user) {
+
+        // Decrypt
+        const bytes = CryptoJS.AES.decrypt(user.password, "#@chaudhary@#");
+        const decryptedPassword = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+        if (user.email == email && password == decryptedPassword) {
+
+            // JWT Token
+            const token = jwt.sign({ email }, "#@chaudhary@#");
+            res.send({
+                success: true,
+                message: "Successfully Logged In",
+                token: token
+            });
+        } else {
+            res.send({ success: false, message: `Invalid Input` });
+        }
+    } else {
+        res.send({ success: false, message: `User Not Found` });
+    }
+})
+
+module.exports = router
